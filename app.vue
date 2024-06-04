3c27c2125668eaf7ef1c0b9c71aeadd48e767289
JakeIsMeh <jakeismeh1@gmail.com>
2024-06-04 21:09:38 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="fonty">I should be Inter</h1>
+</template>
+
+<style>
+:root {
+  color-scheme: dark light;
+}
+
+/* ripped straight from nuxt.com */
+/* On nuxt.com, it falls straight through to system-ui */
+ .fonty {
+    font-family: Inter var experimental, Inter var, Inter, ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+}
+
+</style>
